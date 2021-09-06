b58cc0ddf6f5dbbb158db8b8a67e90e1eafa4240
SigmundGjengedal <sigmund.gjengedal@gmail.com>
2021-09-06 14:01:11 +0200

    Initial commit

---
+++ src/main/java/no/kristiania/App.java
@@ -0,0 +1,8 @@
+package no.kristiania;
+
+public class App {
+
+    public static String convert1ToI(int i) {
+        return "I";
+    }
+}
---
+++ src/test/java/no/kristiania/appTest.java
@@ -0,0 +1,12 @@
+package no.kristiania;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class appTest {
+    @Test
+    void shouldReturnI(){
+        assertEquals("I", App.convert1ToI(1));
+    }
+}
